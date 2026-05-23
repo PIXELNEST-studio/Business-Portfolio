fded850f21e0d9146b999dec0908fc805d631e9b
Pixelnest <chetanyakumar753@gmail.com>
2026-05-23 18:47:38 +0530

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['pixelnest11.netlify.app'],
+  },
+};
+
+module.exports = nextConfig;
